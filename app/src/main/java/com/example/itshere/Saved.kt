6a47de6936ab04dadfd71216b102e2c815d596ce
Cheesepider <jlohziyang2@gmail.com>
2025-12-17 13:29:03 +0800

    Update Saved.kt

--- app/src/main/java/com/example/itshere/Saved.kt
+++ app/src/main/java/com/example/itshere/Saved.kt
@@ -1,6 +1,7 @@
 // SavedScreen.kt
 package com.example.itshere
 
+import android.R.attr.onClick
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
